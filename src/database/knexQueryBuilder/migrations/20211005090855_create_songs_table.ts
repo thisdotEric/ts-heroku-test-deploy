d753c9e49d8ebf455e9c3d8b285eab6dca70727c
thisdotEric <siguenzajohneric@gmail.com>
2021-10-05 19:46:32 +0800

    Connect postgresql db using knex

---
+++ src/database/knexQueryBuilder/migrations/20211005090855_create_songs_table.ts
@@ -0,0 +1,18 @@
+import { Knex } from "knex";
+
+
+export async function up(knex: Knex): Promise<void> {
+	return knex.schema.createTable('songs', table => {
+		table.increments('id').notNullable().primary().unique().defaultTo(0);
+		table.string('name');
+		table.string('artist');
+		table.string('year');
+	})
+
+}
+
+
+export async function down(knex: Knex): Promise<void> {
+	return knex.schema.dropTableIfExists('songs');
+}
+
