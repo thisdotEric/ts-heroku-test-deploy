3a9f95d0c575dd8c7cf1a137b98efd54acdadbca
thisdotEric <siguenzajohneric@gmail.com>
2021-11-30 14:07:36 +0800

    Added home route tests

--- src/server.ts
+++ src/server.ts
@@ -1,10 +1,13 @@
 if (process.env.NODE_ENV !== 'production') require('dotenv').config();
 
-import express from 'express';
+import express, { Response } from 'express';
 import songRoutes from './routes/songs';
 
 const app = express();
 
+app.get('/', (_, res: Response) => {
+  res.status(200).send('Ok');
+});
 app.use('/songs', songRoutes);
 
 export default app;
--- test/server.test.ts
+++ test/server.test.ts
@@ -2,6 +2,11 @@ import request from 'supertest';
 import app from '../src/server';
 
 describe('/songs', () => {
+  it('should return "Ok" on home route', async () => {
+    const res = await request(app).get('/');
+    expect(res.statusCode).toEqual(200);
+    expect(res.body).toEqual({});
+  });
   it('should return arrays of songs', async () => {
     const res = await request(app).get('/songs');
     expect(res.statusCode).toEqual(200);
